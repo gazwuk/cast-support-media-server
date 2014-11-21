f81c07da0bf29a1a973ecd649c3df0cc060d9810
gazwuk <gazwuk@hotmail.com>
2014-11-21 22:59:24 +0000

    Get working with latest version of express

--- s3.js
+++ s3.js
@@ -13,23 +13,20 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  **/
-var express = require("express"),
-    app     = express(),
-    port    = 8080;
+var express = require("express");
+var app     = express();
+var port    = 8080;
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); // NOT SAFE FOR PRODUCTION
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     next();
 }
 
-app.configure(function() {
-    app.use(allowCrossDomain);
-    app.use(express.static(__dirname));
-    app.use(express.directory(__dirname));
-});
+app.use(allowCrossDomain);
+app.use(express.static(__dirname));
+app.use(express.directory(__dirname));
 
 app.listen(port);
